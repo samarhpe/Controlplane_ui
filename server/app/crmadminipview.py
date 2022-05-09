e8cb4fb4163c37fdc408a255fe0f206649faad7c
samarhpe <am-samarasimha.reddy@hpe.com>
2022-05-10 00:00:20 +0530

    ip extracted

--- server/app/crmadminipview.py
+++ server/app/crmadminipview.py
@@ -5,6 +5,7 @@ from kubernetes import client, config
 #from .lib import lib
 import os
 import subprocess
+import re
 from app import main
 # Configs can be set in Configuration class directly or using helper utility
 #config.load_kube_config(config_file=r"./server/app/k3s.yaml")
@@ -15,6 +16,8 @@ class CRMadminView(APIView):
     def get(self, request):
         proc = subprocess.Popen(['crm','configure', 'show' ,'admin-ip' ], stdout=subprocess.PIPE)
         out = proc.communicate()[0]
+        pattern = re.compile(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})')
+        ip=pattern.search(s)[0]
         data={}
-        data["ip"]=out
+        data["ip"]=ip
         return Response(data)
