a3d3384a9ef9b0f6d96d0e460cb31674b6c9cd0c
samarhpe <am-samarasimha.reddy@hpe.com>
2022-05-12 17:40:13 +0530

    2.11 changed to

--- react-sidebar-dropdown/src/pages/CRMclusterpage.js
+++ react-sidebar-dropdown/src/pages/CRMclusterpage.js
@@ -28,7 +28,7 @@ const CRMclusterpage = () => {
          
           >
            <Box >
-             <form action="https://172.30.2.120:7630/login" method="post" target='_blank' id="new_session">
+             <form action="https://172.30.2.11:7630/login" method="post" target='_blank' id="new_session">
                <input name="utf8" type="hidden" value="✓"/>
                <input type="hidden" name="authenticity_token" value="0rn6nUBbcfHqHoMAqr2U9betaip0R0Lu0HpJozP+Pv2S8B8Cq/sKJc00/0rIkwc4JViKZv8/OGyvYrrIlYBb8Q=="/>
               <input type="hidden" name="session[username]" id="session_username" value="hacluster"/>
