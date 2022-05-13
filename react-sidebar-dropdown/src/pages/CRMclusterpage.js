0b0fa188fa42f185b7cc3294a2d6067ac9f257ff
samarhpe <am-samarasimha.reddy@hpe.com>
2022-05-13 14:14:51 +0530

    crm!=null checking

--- react-sidebar-dropdown/src/pages/CRMclusterpage.js
+++ react-sidebar-dropdown/src/pages/CRMclusterpage.js
@@ -17,7 +17,7 @@ const CRMclusterpage = () => {
       //console.log(crm);
       window.open("https://"+crm.ip+":7630/login?username=hacluster&password=linux","_blank")
     }
-    const url="https://"+crm.ip+":7630/login";
+    const url=(crm!=null?"https://"+crm.ip+":7630/login":null);
   return (
     <div>
 <Box  >
@@ -29,7 +29,7 @@ const CRMclusterpage = () => {
          
           >
            <Box >
-             <form action="https://172.30.2.11:7630/login" method="post" target='_blank' id="new_session">
+             <form action={url} method="post" target='_blank' id="new_session">
                <input name="utf8" type="hidden" value="✓"/>
              
               <input type="hidden" name="session[username]" id="session_username" value="hacluster"/>
