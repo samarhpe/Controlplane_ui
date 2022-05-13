ba719d974357dc13fa2a721e4002e342b6cc087d
samarhpe <am-samarasimha.reddy@hpe.com>
2022-05-13 16:35:33 +0530

    type=password

--- react-sidebar-dropdown/src/pages/K3sclusterpage.js
+++ react-sidebar-dropdown/src/pages/K3sclusterpage.js
@@ -14,7 +14,7 @@ const K3sclusterpage = () => {
          
           >
            <Box >
-             <form action="https://172.34.1.109/login" method='post' target='_blank'> 
+             <form action="https://172.34.1.109/#/login" method='post' target='_blank'> 
               <mat-radio-group role="radiogroup" name="login">
               <mat-radio-button _ngcontent-uag-c324="" color="primary" class="mat-radio-button mat-primary mat-radio-checked" id="mat-radio-5">
                 
@@ -23,7 +23,7 @@ const K3sclusterpage = () => {
                 </mat-radio-button>
               </mat-radio-group>
               <mat-form-field>
-                <input _ngcontent-uag-c324 matinput id="token" name="token" type="hidden" value="eyJhbGciOiJSUzI1NiIsImtpZCI6IjBvRllSc241amJ3bFZrek9aT0ROREtMallUQWZndHVld1NXSF93d2laSE0ifQ.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJrdWJlcm5ldGVzLWRhc2hib2FyZCIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VjcmV0Lm5hbWUiOiJhZG1pbi11c2VyLXRva2VuLWY2ZDQ4Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZXJ2aWNlLWFjY291bnQubmFtZSI6ImFkbWluLXVzZXIiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlcnZpY2UtYWNjb3VudC51aWQiOiI5ODBlN2IyMS0yNTc1LTRiMzgtYmM5ZC04M2Y5NDcwYWMxOWEiLCJzdWIiOiJzeXN0ZW06c2VydmljZWFjY291bnQ6a3ViZXJuZXRlcy1kYXNoYm9hcmQ6YWRtaW4tdXNlciJ9.LwEDn7I5u0G8SLc6oyE3_77dropZ0shiR_JcD6YnqOx6rcKDEwqRIvZRwg3mKHrGCXjwPHxbbaJjPSZNlXuWrnaBMRPyOpRnho42e0ulVQXcVA5DUV27vpGK42ItlNdcnIdLd8DhsJrLy5YPgymhOxBlQEOqbw33R6wzFN2p0Jne7lX76sbujBfiIov2e-HUWVXTotyHQZPRhY-9Purats57DVFZSDCBK-dVt2QXcEvez6mjpXzlTOA0yLMNvOic0uPJZsdoondg1XOzfvsC0DQazwWKmRqqno-4f6ou3ljXtMvVyG2KK4VIYoMamuY0vt6e_8NiZ1dhJxtdRS9JEw
+                <input _ngcontent-uag-c324 matinput id="token" name="token" type="password" value="eyJhbGciOiJSUzI1NiIsImtpZCI6IjBvRllSc241amJ3bFZrek9aT0ROREtMallUQWZndHVld1NXSF93d2laSE0ifQ.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJrdWJlcm5ldGVzLWRhc2hib2FyZCIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VjcmV0Lm5hbWUiOiJhZG1pbi11c2VyLXRva2VuLWY2ZDQ4Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZXJ2aWNlLWFjY291bnQubmFtZSI6ImFkbWluLXVzZXIiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlcnZpY2UtYWNjb3VudC51aWQiOiI5ODBlN2IyMS0yNTc1LTRiMzgtYmM5ZC04M2Y5NDcwYWMxOWEiLCJzdWIiOiJzeXN0ZW06c2VydmljZWFjY291bnQ6a3ViZXJuZXRlcy1kYXNoYm9hcmQ6YWRtaW4tdXNlciJ9.LwEDn7I5u0G8SLc6oyE3_77dropZ0shiR_JcD6YnqOx6rcKDEwqRIvZRwg3mKHrGCXjwPHxbbaJjPSZNlXuWrnaBMRPyOpRnho42e0ulVQXcVA5DUV27vpGK42ItlNdcnIdLd8DhsJrLy5YPgymhOxBlQEOqbw33R6wzFN2p0Jne7lX76sbujBfiIov2e-HUWVXTotyHQZPRhY-9Purats57DVFZSDCBK-dVt2QXcEvez6mjpXzlTOA0yLMNvOic0uPJZsdoondg1XOzfvsC0DQazwWKmRqqno-4f6ou3ljXtMvVyG2KK4VIYoMamuY0vt6e_8NiZ1dhJxtdRS9JEw
 "/>
               </mat-form-field>
               <input type="submit" value="Click here to go to kubernetes dashboard"/>
