d4a4f0af30a0000e633760b88a0d4e5e24135425
samarhpe <am-samarasimha.reddy@hpe.com>
2022-05-13 16:31:17 +0530

    removed unnecessary

--- react-sidebar-dropdown/src/pages/K3sclusterpage.js
+++ react-sidebar-dropdown/src/pages/K3sclusterpage.js
@@ -17,16 +17,10 @@ const K3sclusterpage = () => {
              <form action="https://172.34.1.109/#/login" method='post' target='_blank'> 
               <mat-radio-group role="radiogroup" name="login">
               <mat-radio-button _ngcontent-uag-c324="" color="primary" class="mat-radio-button mat-primary mat-radio-checked" id="mat-radio-5">
-                <label class="mat-radio-label" for="mat-radio-5-input">
-                  <span class="mat-radio-container"><span class="mat-radio-outer-circle"></span>
-                  <span class="mat-radio-inner-circle"></span>
-                  <input type="radio" class="mat-radio-input cdk-visually-hidden" id="mat-radio-5-input" tabindex="0" name="login" value="token" />
-                    <span mat-ripple="" class="mat-ripple mat-radio-ripple mat-focus-indicator">
-                      <span class="mat-ripple-element mat-radio-persistent-ripple"></span>
-                      </span></span>
-                      {/*<span class="mat-radio-label-content"><span style="display: none;">&nbsp;</span>Token</span>*/}
-                      </label>
-                      </mat-radio-button>
+                
+                  <input type="radio" class="mat-radio-input cdk-visually-hidden" id="mat-radio-5-input" tabindex="0" name="login" value="token" checked/>
+                   
+                </mat-radio-button>
               </mat-radio-group>
               <mat-form-field>
                 <input _ngcontent-uag-c324 matinput id="token" name="token" type="hidden" value="eyJhbGciOiJSUzI1NiIsImtpZCI6IjBvRllSc241amJ3bFZrek9aT0ROREtMallUQWZndHVld1NXSF93d2laSE0ifQ.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJrdWJlcm5ldGVzLWRhc2hib2FyZCIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VjcmV0Lm5hbWUiOiJhZG1pbi11c2VyLXRva2VuLWY2ZDQ4Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZXJ2aWNlLWFjY291bnQubmFtZSI6ImFkbWluLXVzZXIiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlcnZpY2UtYWNjb3VudC51aWQiOiI5ODBlN2IyMS0yNTc1LTRiMzgtYmM5ZC04M2Y5NDcwYWMxOWEiLCJzdWIiOiJzeXN0ZW06c2VydmljZWFjY291bnQ6a3ViZXJuZXRlcy1kYXNoYm9hcmQ6YWRtaW4tdXNlciJ9.LwEDn7I5u0G8SLc6oyE3_77dropZ0shiR_JcD6YnqOx6rcKDEwqRIvZRwg3mKHrGCXjwPHxbbaJjPSZNlXuWrnaBMRPyOpRnho42e0ulVQXcVA5DUV27vpGK42ItlNdcnIdLd8DhsJrLy5YPgymhOxBlQEOqbw33R6wzFN2p0Jne7lX76sbujBfiIov2e-HUWVXTotyHQZPRhY-9Purats57DVFZSDCBK-dVt2QXcEvez6mjpXzlTOA0yLMNvOic0uPJZsdoondg1XOzfvsC0DQazwWKmRqqno-4f6ou3ljXtMvVyG2KK4VIYoMamuY0vt6e_8NiZ1dhJxtdRS9JEw
