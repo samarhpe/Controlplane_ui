4ed9a68320fccc32bd109918dba1392cea5704d8
samarhpe <am-samarasimha.reddy@hpe.com>
2022-05-17 19:19:59 +0530

    style added

--- react-sidebar-dropdown/src/pages/K3sclusterpage.js
+++ react-sidebar-dropdown/src/pages/K3sclusterpage.js
@@ -93,22 +93,25 @@ const K3sclusterpage = () => {
                  
                      
                      <CardBody pad="small">
+                       <div style="width:100%;">
                        <form align="center" method="post" action="https://172.34.1.102/api/v1/session" target='_blank'>
                        <input name="utf8" type="hidden" value="✓"/>
                          <input type="hidden" name="username" id="username" value="admin"/>
                          <input type="hidden" name="password" id="password" value="Password!234"/>
-                    <Button margin="small" width="50%" type="submit" primary label="ArgoCD" />
+                    <Button margin="small" type="submit" primary label="ArgoCD" />
                      </form>
-                     
+                     </div>
                      <Button margin="small" primary onClick={()=>window.open("https://argo-cd.readthedocs.io/en/stable/","_blank")} label="Pulp"/>
                      
                     {/* <Button margin="small" primary onClick={()=>window.open("https://argo-cd.readthedocs.io/en/stable/","_blank")} label="Harbor"/>*/}
+                    <div style="width:100%">
                     <form align="center" method="post" action="https://172.34.1.110/harbor/sign-in?redirect_url=%2Fharbor%2Fprojects" target='_blank'>
                        <input name="utf8" type="hidden" value="✓"/>
                          <input type="hidden" name="login_username" id="login_username" value="admin"/>
                          <input type="hidden" name="login_password" id="login_password" value="Password!234"/>
-                    <Button margin="small" width="50%" primary type="submit" label="Harbor"/>
+                    <Button margin="small" primary type="submit" label="Harbor"/>
                      </form>
+                     </div>
                     
                      <Button margin="small" primary onClick={()=>window.open("https://argo-cd.readthedocs.io/en/stable/","_blank")} label="Prometheus"/>
                      </CardBody>
